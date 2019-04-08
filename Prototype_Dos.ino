151a5c6e27b10cd4cc401cf2d9f8e54414ea8a52
bharath624 <48464831+bharath624@users.noreply.github.com>
2019-04-08 12:05:27 -0400

    Add files via upload

---
+++ Prototype_Dos.ino
@@ -0,0 +1,24 @@
+#include "HX711.h"
+
+// HX711 circuit wiring
+const int LOADCELL_DOUT_PIN = 2;
+const int LOADCELL_SCK_PIN = 3;
+
+HX711 scale;
+
+void setup() {
+  Serial.begin(57600);
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+}
+
+void loop() {
+
+  if (scale.is_ready()) {
+    long reading = scale.read();
+    Serial.print("Sensosr reading: ");
+    Serial.println(reading);
+  } else {
+    Serial.println("Sensor not found.");
+  }
+  delay(1000);
+}
